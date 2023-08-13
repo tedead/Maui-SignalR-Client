02c0261a32588810346e34315e052a7f3a283613
tedead <jhalv27@gmail.com>
2023-08-13 17:48:06 -0500

    new

--- Communicator/MainPage.xaml.cs
+++ Communicator/MainPage.xaml.cs
@@ -7,21 +7,19 @@ public partial class MainPage : ContentPage
 {
 	int count = 0;
     private HubConnection hubConnection;
+
     public MainPage()
 	{
 		InitializeComponent();
 
-        hubConnection = new HubConnectionBuilder()
-    .WithUrl("http://192.168.1.128:5043/chatHub")
-    //.WithUrl("https://192.168.1.128:7174")
-    .Build();
-        hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+        try
         {
-            Debug.WriteLine(user, message);
-            messageList.Text = $"{message}";
-        });
-
-        Task.Run(() => { Dispatcher.Dispatch(async () => await hubConnection.StartAsync()); });
+            Connect();
+        }
+        catch(Exception ex) 
+        {
+            Debug.WriteLine(string.Format("Error connecting: {0}", ex.Message));
+        }
     }
 
     private void ConnectClicked(object sender, EventArgs e)
@@ -32,28 +30,50 @@ public partial class MainPage : ContentPage
 
     private void SendClicked(object sender, EventArgs e)
     {
-		string msg = message.Text;
-		string u = name.Text;
-        _ = SendMessage(msg, u);
-
-        SemanticScreenReader.Announce("Sent");
+        _ = SendMessage();
     }
 
-    public Task Connect()
-    {
-        return hubConnection.StartAsync();
-    }
+    //public Task Connect()
+    //{
+    //    return hubConnection.StartAsync();
+    //}
 
     public Task Disconnect()
     {
         return hubConnection.StopAsync();
     }
 
-    public async Task SendMessage(string user, string message)
+    public async Task SendMessage()
     {
-        //var a = hubConnection.ConnectionId;
-        //var b = hubConnection.State;
-        await hubConnection.InvokeAsync("SendMessage", user, message);
+
+        if(hubConnection == null || hubConnection.ConnectionId == null)
+        {
+            try
+            {
+                Connect();
+            }
+            catch(Exception ex) 
+            {
+                Debug.WriteLine(string.Format("Error connecting: {0}", ex.Message));
+            }
+        }
+
+        await hubConnection.InvokeAsync("SendMessage", name.Text, message.Text);
+    }
+
+    public void Connect()
+    {
+        hubConnection = new HubConnectionBuilder()
+            .WithUrl("http://192.168.1.128:5043/chatHub")
+            //.WithUrl("https://192.168.1.128:7174")
+            .Build();
+        hubConnection.On<string, string>("ReceiveMessage", (user, message) =>
+        {
+            Debug.WriteLine(user, message);
+            messageList.Text += $"{message}";
+        });
+
+        Task.Run(() => { Dispatcher.Dispatch(async () => await hubConnection.StartAsync()); });
     }
 }
 
